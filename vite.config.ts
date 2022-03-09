b0a685b64d991087f5ead66065fd7c1dc689123e
EstaniPP <estanipp@gmail.com>
2022-03-09 15:59:19 -0300

    vite config changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import base from '@emotion/styled/types/base'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true
-  }
+  },
+  base: '/CILevelCreator/'
 })
